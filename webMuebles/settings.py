793bfcbf785c6f674c99a1984ad97a33b549fd3f
Cristóbal Barrera <cristobal.andres016@gmail.com>
2025-09-09 09:43:43 -0300

    holas

--- webMuebles/settings.py
+++ webMuebles/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-#hola soy cristobal
+#hola soy cristobal probannndoooo
 #hola soy brandon PROBANDO PULL TESSSSTTT ACEPTA CAMBIOOOOS UNA VEZ MASSSSS
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
